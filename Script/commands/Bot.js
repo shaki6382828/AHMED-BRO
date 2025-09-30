4a186a52a7cd43e7fada865d1489524681e872d3
mdshifat-goat <eeeqer3@gmail.com>
2025-09-30 08:12:06 +0600

    Update Bot.js

--- Script/commands/Bot.js
+++ Script/commands/Bot.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "goibot",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "Fixed By Arun Kumar",
+  credits: "shifat",
   description: "goibot",
   commandCategory: "Noprefix",
   usages: "noprefix",
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `✨${name}✨,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : ཫ≛⃝SHIFAT `
+      body: `✨${name}✨,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : ཫ≛⃝sithil ahmed shakib `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
